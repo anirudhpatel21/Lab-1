d60fa5a6e5975d862f7e9584c0c81a64d67ef95e
anirudhpatel21 <anirudh.patel@mail.utoronto.ca>
2026-05-07 20:07:23 -0400

    Adding Multiples file

---
+++ src/Multiples.java
@@ -0,0 +1,31 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+        int counter = 0;
+        while (i < 1000) {
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                counter += 1;
+
+            } else if (divisibleBy3) {
+
+                counter += 1;
+
+            } else if (divisibleBy5) {
+
+                counter += 1;
+
+            } else {
+
+                counter += 0;
+            }
+            i += 1;
+        }
+        System.out.println(counter);
+    }
+}
